3c496bc3aa79d9c3a876f7ec8050af55a79785e2
gyro永不抽风 <1247006353@qq.com>
2023-03-10 00:50:47 -0500

    feat: add generation batch script

---
+++ batch_generate_script_cmd_32.bat
@@ -0,0 +1,32 @@
+start " 1" cmd /K python font_ds_generate_script.py 1 32
+start " 2" cmd /K python font_ds_generate_script.py 2 32
+start " 3" cmd /K python font_ds_generate_script.py 3 32
+start " 4" cmd /K python font_ds_generate_script.py 4 32
+start " 5" cmd /K python font_ds_generate_script.py 5 32
+start " 6" cmd /K python font_ds_generate_script.py 6 32
+start " 7" cmd /K python font_ds_generate_script.py 7 32
+start " 8" cmd /K python font_ds_generate_script.py 8 32
+start " 9" cmd /K python font_ds_generate_script.py 9 32
+start "10" cmd /K python font_ds_generate_script.py 10 32
+start "11" cmd /K python font_ds_generate_script.py 11 32
+start "12" cmd /K python font_ds_generate_script.py 12 32
+start "13" cmd /K python font_ds_generate_script.py 13 32
+start "14" cmd /K python font_ds_generate_script.py 14 32
+start "15" cmd /K python font_ds_generate_script.py 15 32
+start "16" cmd /K python font_ds_generate_script.py 16 32
+start "17" cmd /K python font_ds_generate_script.py 17 32
+start "18" cmd /K python font_ds_generate_script.py 18 32
+start "19" cmd /K python font_ds_generate_script.py 19 32
+start "20" cmd /K python font_ds_generate_script.py 20 32
+start "21" cmd /K python font_ds_generate_script.py 21 32
+start "22" cmd /K python font_ds_generate_script.py 22 32
+start "23" cmd /K python font_ds_generate_script.py 23 32
+start "24" cmd /K python font_ds_generate_script.py 24 32
+start "25" cmd /K python font_ds_generate_script.py 25 32
+start "26" cmd /K python font_ds_generate_script.py 26 32
+start "27" cmd /K python font_ds_generate_script.py 27 32
+start "28" cmd /K python font_ds_generate_script.py 28 32
+start "29" cmd /K python font_ds_generate_script.py 29 32
+start "30" cmd /K python font_ds_generate_script.py 30 32
+start "31" cmd /K python font_ds_generate_script.py 31 32
+start "32" cmd /K python font_ds_generate_script.py 32 32
---
+++ batch_generate_script_cmd_64.bat
@@ -0,0 +1,64 @@
+start " 1" cmd /K python font_ds_generate_script.py 1 64
+start " 2" cmd /K python font_ds_generate_script.py 2 64
+start " 3" cmd /K python font_ds_generate_script.py 3 64
+start " 4" cmd /K python font_ds_generate_script.py 4 64
+start " 5" cmd /K python font_ds_generate_script.py 5 64
+start " 6" cmd /K python font_ds_generate_script.py 6 64
+start " 7" cmd /K python font_ds_generate_script.py 7 64
+start " 8" cmd /K python font_ds_generate_script.py 8 64
+start " 9" cmd /K python font_ds_generate_script.py 9 64
+start "10" cmd /K python font_ds_generate_script.py 10 64
+start "11" cmd /K python font_ds_generate_script.py 11 64
+start "12" cmd /K python font_ds_generate_script.py 12 64
+start "13" cmd /K python font_ds_generate_script.py 13 64
+start "14" cmd /K python font_ds_generate_script.py 14 64
+start "15" cmd /K python font_ds_generate_script.py 15 64
+start "16" cmd /K python font_ds_generate_script.py 16 64
+start "17" cmd /K python font_ds_generate_script.py 17 64
+start "18" cmd /K python font_ds_generate_script.py 18 64
+start "19" cmd /K python font_ds_generate_script.py 19 64
+start "20" cmd /K python font_ds_generate_script.py 20 64
+start "21" cmd /K python font_ds_generate_script.py 21 64
+start "22" cmd /K python font_ds_generate_script.py 22 64
+start "23" cmd /K python font_ds_generate_script.py 23 64
+start "24" cmd /K python font_ds_generate_script.py 24 64
+start "25" cmd /K python font_ds_generate_script.py 25 64
+start "26" cmd /K python font_ds_generate_script.py 26 64
+start "27" cmd /K python font_ds_generate_script.py 27 64
+start "28" cmd /K python font_ds_generate_script.py 28 64
+start "29" cmd /K python font_ds_generate_script.py 29 64
+start "30" cmd /K python font_ds_generate_script.py 30 64
+start "31" cmd /K python font_ds_generate_script.py 31 64
+start "32" cmd /K python font_ds_generate_script.py 32 64
+start "33" cmd /K python font_ds_generate_script.py 33 64
+start "34" cmd /K python font_ds_generate_script.py 34 64
+start "35" cmd /K python font_ds_generate_script.py 35 64
+start "36" cmd /K python font_ds_generate_script.py 36 64
+start "37" cmd /K python font_ds_generate_script.py 37 64
+start "38" cmd /K python font_ds_generate_script.py 38 64
+start "39" cmd /K python font_ds_generate_script.py 39 64
+start "40" cmd /K python font_ds_generate_script.py 40 64
+start "41" cmd /K python font_ds_generate_script.py 41 64
+start "42" cmd /K python font_ds_generate_script.py 42 64
+start "43" cmd /K python font_ds_generate_script.py 43 64
+start "44" cmd /K python font_ds_generate_script.py 44 64
+start "45" cmd /K python font_ds_generate_script.py 45 64
+start "46" cmd /K python font_ds_generate_script.py 46 64
+start "47" cmd /K python font_ds_generate_script.py 47 64
+start "48" cmd /K python font_ds_generate_script.py 48 64
+start "49" cmd /K python font_ds_generate_script.py 49 64
+start "50" cmd /K python font_ds_generate_script.py 50 64
+start "51" cmd /K python font_ds_generate_script.py 51 64
+start "52" cmd /K python font_ds_generate_script.py 52 64
+start "53" cmd /K python font_ds_generate_script.py 53 64
+start "54" cmd /K python font_ds_generate_script.py 54 64
+start "55" cmd /K python font_ds_generate_script.py 55 64
+start "56" cmd /K python font_ds_generate_script.py 56 64
+start "57" cmd /K python font_ds_generate_script.py 57 64
+start "58" cmd /K python font_ds_generate_script.py 58 64
+start "59" cmd /K python font_ds_generate_script.py 59 64
+start "60" cmd /K python font_ds_generate_script.py 60 64
+start "61" cmd /K python font_ds_generate_script.py 61 64
+start "62" cmd /K python font_ds_generate_script.py 62 64
+start "63" cmd /K python font_ds_generate_script.py 63 64
+start "64" cmd /K python font_ds_generate_script.py 64 64
