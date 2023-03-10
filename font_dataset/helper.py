8f19579d60c1ea006bfdeb8fb53f961384d48b76
gyro永不抽风 <1247006353@qq.com>
2023-03-10 00:47:28 -0500

    fix: bug fix in `char_in_font` detection

--- font_dataset/helper.py
+++ font_dataset/helper.py
@@ -5,9 +5,12 @@ __all__ = ["char_in_font"]
 
 
 def char_in_font(unicode_char, font_path):
-    font = TTFont(font_path)
-    for cmap in font["cmap"].tables:
-        if cmap.isUnicode():
-            if ord(unicode_char) in cmap.cmap:
-                return True
-    return False
+    try:
+        font = TTFont(font_path, fontNumber=0)
+        for cmap in font["cmap"].tables:
+            if cmap.isUnicode():
+                if ord(unicode_char) in cmap.cmap:
+                    return True
+        return False
+    except Exception as e:
+        return False
